40c7de6dd1544b8469ae87ced861a8f4a56fe5f4
UncertainWorld <159841920+BlueSky613@users.noreply.github.com>
2025-10-30 10:49:43 -0400

    Change bottomnavigationbaritem height 25
    Increase the height of the icon in the BottomNavigationBarItem.

--- lib/screens/homemain/homemain.dart
+++ lib/screens/homemain/homemain.dart
@@ -93,7 +93,7 @@ class HomeMainState extends State<HomeMain> {
             ),
             BottomNavigationBarItem(
               icon: SizedBox(
-                  height: 22,
+                  height: 25,
                   width: 22,
                   child: SvgPicture.asset(
                     'assets/images/svg/recipie.svg',
