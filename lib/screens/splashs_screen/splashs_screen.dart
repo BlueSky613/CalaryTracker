0fee14554582c2830845f81f6d5fe5e8004d6646
UncertainWorld <159841920+BlueSky613@users.noreply.github.com>
2025-12-31 00:42:16 -0500

    change startTime duration seconds 6

--- lib/screens/splashs_screen/splashs_screen.dart
+++ lib/screens/splashs_screen/splashs_screen.dart
@@ -25,7 +25,7 @@ class _SplashsScreenState extends State<SplashsScreen> {
   }
 
   startTime() async {
-    var duration = const Duration(seconds: 5);
+    var duration = const Duration(seconds: 6);
     return Timer(duration, showAd);
   }
 
