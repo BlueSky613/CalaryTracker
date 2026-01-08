0b3548c453145a6fc9bb23597656debd130a447a
UncertainWorld <159841920+BlueSky613@users.noreply.github.com>
2026-01-08 07:06:18 -0500

    change LinearprogressState value 40

--- lib/widgets/linearprogress.dart
+++ lib/widgets/linearprogress.dart
@@ -27,7 +27,7 @@ class LinearProgress extends StatefulWidget {
 }
 
 class _LinearProgressState extends State<LinearProgress> {
-  double? _value = 50;
+  double? _value = 40;
 
 @override
   void initState() {
